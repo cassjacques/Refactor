9f8a92f8dea61ecbb111e98c26cfee313cd903aa
kbbushman <k.bushman@yahoo.com>
2021-02-10 16:22:05 -0800

    Adds demo code

---
+++ monument-react/src/components/Landing.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Archive from './Archive';
+
+import LandingCSS from './Landing.css';
+
+const Landing = () => {
+  return (
+    <>
+      <Header />
+      <Archive />
+    </>
+  );
+};
+
+export default Landing;
